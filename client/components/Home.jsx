8a442696d881c00029c723465b6978e170cd6abd
Kat Robbie <katherine.robbie@gmail.com>
2017-05-05 16:34:31 +1200

    aligning wood image and text

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Timber</h1>
-      <h2>Wood sourcing in the Bay of Plenty</h2>
-        <div className="wood-header">
-            <img src="https://static.pexels.com/photos/1765/wood-tree-grain-fallen.jpg" alt="Woodgrain Header" />
-          </div>
+      <div>
+        <img src="https://static.pexels.com/photos/1765/wood-tree-grain-fallen.jpg" alt="Woodgrain Header" className="wood-header"/>
+          <h1>Timber</h1>
+          <h2>Sourcing wood in the Bay of Plenty</h2>
+      </div>
     </div>
   )
 }
