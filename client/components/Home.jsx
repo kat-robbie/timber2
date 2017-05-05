5b5a1a9115d97c2cc9f8b331f741400379e5dbff
Kat Robbie <katherine.robbie@gmail.com>
2017-05-05 16:07:45 +1200

    added wood header to images

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
     <div>
       <h1>Timber</h1>
       <h2>Wood sourcing in the Bay of Plenty</h2>
+        <div className="header-photo">
+            <img src="wood-tree-grain-fallen.jpg"/>
+          </div>
     </div>
   )
 }
