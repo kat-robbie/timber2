b47cd751eb3cc9c8781c0f190c617ca14c77adc0
Kat Robbie <katherine.robbie@gmail.com>
2017-05-07 17:16:25 +1200

    added css

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Home from './Home'
+import Nav from './Nav'
 
 function App () {
   return (
     <div className='app-container'>
       <Home />
+      <Nav />
     </div>
   )
 }
