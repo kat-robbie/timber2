c3d795299e8a95e3a6d79e195b51258dfd98e4eb
Kat Robbie <katherine.robbie@gmail.com>
2017-05-09 11:06:22 +1200

    got text over image

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,11 +4,9 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="header">
-      <img className="wood-header" src="https://static.pexels.com/photos/1765/wood-tree-grain-fallen.jpg" alt="Woodgrain Header" />
-        <div className="text">
+      <img src="https://static.pexels.com/photos/1765/wood-tree-grain-fallen.jpg" alt="Woodgrain Header" />
             <h1>Real Wood Products</h1>
             <h2>Milling and crafting timber in New Zealand</h2>
-        </div>
     </div>
   )
 }
