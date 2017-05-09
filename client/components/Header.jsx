d6c993fc6cabb8a471c44135d040097558314752
Kat Robbie <katherine.robbie@gmail.com>
2017-05-10 10:56:55 +1200

    more css tinkering

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="header">
-      <img className="wood-header" src="https://static.pexels.com/photos/1765/wood-tree-grain-fallen.jpg" alt="Woodgrain Header" />
+      <img className="wood-image" src="https://static.pexels.com/photos/1765/wood-tree-grain-fallen.jpg" alt="Woodgrain Header" />
             <h1>Real Wood Products</h1>
             <h2>Milling and crafting timber in New Zealand</h2>
     </div>
