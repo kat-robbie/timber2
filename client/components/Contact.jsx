f973febdf9bc32180eee90a86f03bd2256996016
Kat Robbie <katherine.robbie@gmail.com>
2017-05-14 20:56:26 +1200

    playing with js mail protector

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -8,4 +8,12 @@ const Contact = () => {
     </div>
   )
 }
+
+function newContent() {
+    alert("load new content");
+    document.open();
+    document.write("<h1>Out with the old - in with the new!</h1>");
+    document.close();
+  }
+  
 export default Contact
