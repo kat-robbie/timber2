1747a85dc69030799779fb906d2cf266af881cf5
Kat Robbie <katherine.robbie@gmail.com>
2017-05-18 17:22:32 +1200

    email var css responding

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -5,16 +5,12 @@ const Contact = () => {
   return (
     <div className="contact">
       Contact
-      <div>
-        <img src="https://github.com/kat-robbie/timber2/blob/master/public/images/email.gif?raw=true" alt="Woodgrain Header" />
-      </div>
     </div>
   )
 }
 
-var emailAddress = ('ianrobbie@' + 'xtra.co.nz');
-document.write('<a href="mailto:' + emailAddress + '">' + emailAddress + '</a>');
-
+var emailAddress = ('ianrobbie@' + 'xtra.co.nz')
+document.write('<h1>' + '<a href="mailto:' + emailAddress + '">' + emailAddress + '</a>' + '</h1>')
 
 
 
