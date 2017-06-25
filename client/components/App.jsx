abc345d3ffe76fe8365f7aa507def96b7f270dfd
Kat Robbie <katherine.robbie@gmail.com>
2017-06-25 21:16:19 +1200

    nav bar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -14,6 +14,7 @@ import Testimonials from './Testimonials'
 function App () {
   return (
     <div className="app">
+      <Nav />
 
       <div className="topdivider"></div>
 
@@ -33,7 +34,6 @@ function App () {
 }
 
 export default App
-
 
 // <Router>
 //   <div>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -5,15 +5,16 @@ import { HashRouter as Router, Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>
-      <Router>
-      <ul className="nav">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/gallery">Gallery</Link></li>
-        <li><Link to="contact">Contact</Link></li>
-      </ul>
-      </Router>
+    <div className="nav">
+      <div className="container">
+        <Router>
+          <ul className="list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/gallery">Gallery</Link></li>
+            <li><Link to="contact">Contact</Link></li>
+          </ul>
+        </Router>
+      </div>
     </div>
   )
 }
